f3a8469dba0e7cbc3c4d0d3c00ad037688dc3a5b
Yusif Huseynov <hv.yusif@mail.ru>
2018-12-31 13:04:01 +0400

    no message

--- VKE/ViewController.swift
+++ VKE/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var boyu = 0.0,kilo:Double = 0.0
 
+    @IBAction func boy(_ sender: UITextField) {
+        boyu=boy.(<#T##sender: UITextField##UITextField#>)
+    }
+    
+    @IBAction func kilo(_ sender: UITextField) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
